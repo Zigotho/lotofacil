5a1b4215f2078e8469ce2d085a3b1857cbf00389
Matheus de Carvalho <matheusc.j@hotmail.com>
2024-02-20 15:15:14 -0300

    chore: create two routes to test

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,22 @@
-console.log('teste')
+import express from 'express'
+
+import { Router, Request, Response } from 'express';
+
+const app = express();
+
+const route = Router()
+
+app.use(express.json())
+
+route.get('/', (req: Request, res: Response) => {
+  res.json({ message: 'hello world with Typescripts' })
+})
+
+route.get('/results', (req: Request, res: Response) => {
+  res.json({ message: 'Vou devolver a planilha aqui' })
+})
+
+app.use(route)
+
+
+app.listen(3333, () => 'server running on port 3333')
